4e596b6253a99b1f0438ca4be8fbe55e37a1154b
Nathan Jacobson <95062977+grqphical@users.noreply.github.com>
2025-11-06 10:42:33 -0800

    added simple database to track whose in dining commons

---
+++ cogs/database.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+
+class Database(commands.Cog):
+    def __init__(self):
+        super().__init__()
+        self.store = {}
+    
+    def set(self, user: discord.User, in_dining_commons: bool):
+        self.store[user.id] = in_dining_commons
+    
+    def get(self, user: discord.User) -> bool:
+        return self.store[user.id]
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Database())
